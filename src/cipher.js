49edb97038a077a391dbeda81c67cde04b1a2cf9
PriTheodoro <pri.paf@gmail.com>
2019-01-30 13:19:02 -0200

    js completo e início de html

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,53 @@
-window.cipher = {
-  // ... 
-};
+
+function enterText()
+
+
+function encode(setenceBeCode, desloc){
+    let codLetraAscII = '';
+    let setenceNew = '';
+    
+    for(let i = 0; i < setenceBeCode.length; i++){
+    codLetraAscII = setenceBeCode.charCodeAt(i); 
+     let setenceFormCxAlta = '';
+     let setenceFormCxBaixa = '';
+      if (setenceBeCode.charCodeAt(i) >= 65 && setenceBeCode.charCodeAt(i) <= 90){ setenceFormCxAlta  = ((codLetraAscII - 65 + (desloc % 26 ) + 26 ) % 26 + 65);
+      
+      } else if (setenceBeCode.charCodeAt(i) >= 97 && setenceBeCode.charCodeAt(i) <= 122){ setenceFormCxBaixa = ((codLetraAscII - 97 + ( desloc % 26 ) + 26 ) % 26 + 97);
+      } else { setenceNew += String.fromCharCode(codLetraAscII) 
+      }
+      
+    setenceNew += String.fromCharCode(setenceFormCxAlta, setenceFormCxBaixa);
+    
+    
+    }
+    console.log(setenceNew);
+    return setenceNew;
+        
+    }
+    const cifrar = encode('Olá, tudo bem?', 1)
+    console.log(cifrar);
+    
+    
+    function decode(setenceBeCode, desloc){
+    let codLetraAscII = '';
+    let setenceNew = '';
+    
+    for(let i = 0; i < setenceBeCode.length; i++){
+    codLetraAscII = setenceBeCode.charCodeAt(i); 
+     let setenceFormCxAlta = '';
+     let setenceFormCxBaixa = '';
+      if (setenceBeCode.charCodeAt(i) >= 65 && setenceBeCode.charCodeAt(i) <= 90){ setenceFormCxAlta  = ((codLetraAscII - 65 - (desloc % 26) + 26) % 26 + 65);
+      
+      } else if (setenceBeCode.charCodeAt(i) >= 97 && setenceBeCode.charCodeAt(i) <= 122){ setenceFormCxBaixa = ((codLetraAscII - 97 - (desloc % 26) + 26) % 26 + 97);
+      } else { setenceNew += String.fromCharCode(codLetraAscII) 
+      }
+      
+    setenceNew += String.fromCharCode(setenceFormCxAlta, setenceFormCxBaixa);
+        
+    }
+    console.log(setenceNew);
+    return setenceNew;
+         
+    }
+    const decifrar = decode('Pmá, uvep cfn?', 1)
+    console.log(decifrar);
